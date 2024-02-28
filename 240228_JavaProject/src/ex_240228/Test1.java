80a284a7e7ea5ddbb214deed4bb0d7b419748799
seohyun1001 <sh54991210@gmail.com>
2024-02-28 10:30:15 +0900

    first commit

---
+++ 240228_JavaProject/src/ex_240228/Test1.java
@@ -0,0 +1,14 @@
+package ex_240228;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World~!!");
+		// 역할 : 콘솔에 메세지를 출력 - 매우 중요!
+		// 프로그래밍에서 항상 값을 잘 입력, 출력 되었는지
+		// 반드시 확인해야 함. 확인하는 도구로 사용.
+	}
+
+}
