2d3822f2df40939b72dfb3fcae22b9e5215a8397
seohyun1001 <sh54991210@gmail.com>
2024-02-29 16:05:19 +0900

    240229 16:00 update / [java] array & for-each

--- 240228_JavaProject/src/ex_240229/Ex_10_array5.java
+++ 240228_JavaProject/src/ex_240229/Ex_10_array5.java
@@ -4,7 +4,15 @@ public class Ex_10_array5 {
 
 	public static void main(String[] args) {
 
+		int [] array = {1,2,3,4,5};
+		for (int i : array) {
+			System.out.print(i);
+		}
 
+		String [] array2 = {"a", "b", "c", "d", "e"};
+		for (String text : array2) {
+			System.out.print(text);
+		}
 	}
 
 }
