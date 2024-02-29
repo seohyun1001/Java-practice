83295781d681eac2faf56391f386353ee899a6db
seohyun1001 <sh54991210@gmail.com>
2024-02-29 17:33:16 +0900

    .

--- 240228_JavaProject/src/ex_240229/Ex_10_array5.java
+++ 240228_JavaProject/src/ex_240229/Ex_10_array5.java
@@ -5,10 +5,12 @@ public class Ex_10_array5 {
 	public static void main(String[] args) {
 
 		int [] array = {1,2,3,4,5};
-		for (int i : array) {
-			System.out.print(i);
+		System.out.println("for each 예제1");
+		for(int i : array) {
+			System.out.print(i +", ");
 		}
-
+		System.out.println();
+		System.out.println("for each 예제2");
 		String [] array2 = {"a", "b", "c", "d", "e"};
 		for (String text : array2) {
 			System.out.print(text);
