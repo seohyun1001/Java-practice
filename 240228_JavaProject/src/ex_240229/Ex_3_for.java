c0fa679889d0c4c61307c19c0116f8fe31d45077
seohyun1001 <sh54991210@gmail.com>
2024-02-29 11:19:24 +0900

    240229 11:20 update / [java] for + while basic

--- 240228_JavaProject/src/ex_240229/Ex_3_for.java
+++ 240228_JavaProject/src/ex_240229/Ex_3_for.java
@@ -5,23 +5,40 @@ import java.util.Scanner;
 public class Ex_3_for {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("입력한 n까지 더하는 프로그램 / 숫자를 입력해주세요 >>");
-		int number = scanner.nextInt();
-		int result = sumN(number);
-		System.out.println("결과는 : " + result);
+		while (true) {
+			// while문은 보통은 무언가 계속 실행하게 하고,
+			// 특정 조건에서 빠져나올 때 많이 사용함.
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("입력한 n까지 더하는 프로그램");
+			System.out.println("1 : 숫자 입력 / 2 : 종료) >>");
+			int number = scanner.nextInt();
+			
+			// 조건문 분기
+			if (number == 1) {
+				System.out.println("숫자를 입력해주세요 >>");
+				int number2 = scanner.nextInt();
+				int result = sumN(number2);
+				System.out.println("결과는 : " + result);
+			} else if (number == 2) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else {
+				System.out.println("*주의) 숫자는 1 또는 2 중에서 입력해주세요.");
+			}
 		}
+
+	}
 
 	// 입력한 n까지 더하는 프로그램 만들기 1+2+3+...+n
 	public static int sumN(int number) {
 		// 초기값, 합의 값을 할당
 		int i, sum = 0;
-		for(i=0; i<=number; i++) {
+		for (i = 0; i <= number; i++) {
 			// 초기값 : i=0;
 			// 조건 : i가 number(입력한 값)가 될 때까지;
 			// 반복 후 작업 : i + 1
 			sum += i;
-			// 반복문 : 
+			// 반복문 :
 		}
 		return sum;
 	}
