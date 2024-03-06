ce7f3915246bea521bb0aa75825e6e107edabbbd
seohyun1001 <sh54991210@gmail.com>
2024-03-06 16:27:19 +0900

    16:20 / [java] multiple inheritance of interface

---
+++ 240228_JavaProject/src/ex_240306/Ex_06_interface_subClass_2.java
@@ -0,0 +1,25 @@
+package ex_240306;
+
+public class Ex_06_interface_subClass_2 implements test1 {
+
+	@Override
+	public void showMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showDrinkMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showSnackMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
--- 240228_JavaProject/src/ex_240306/test1.java
+++ 240228_JavaProject/src/ex_240306/test1.java
@@ -5,5 +5,9 @@ public interface test1 extends test2, test3 {
 	// 1) 상수, 2) 추상메소드
 	String snackMenu = "nacho";
 	void showSnackMenu();
+	
+	// test1 인터페이스에 여러가지 많은 기능을 다 넣으면 유지보수가 어려움
+	// -> 상속으로 해당 기능을 다 묶을 수 있지만
+	// 유지보수 등을 고려하여 느슨한 결합으로 설계
 
 }
