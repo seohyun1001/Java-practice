84723d7e8365c68f9bfc3a0f580f6170c8817b4d
seohyun1001 <sh54991210@gmail.com>
2024-03-06 15:04:55 +0900

    240306 15:00 update / [java] inheritance basic_7(abstract_2)

--- 240228_JavaProject/src/ex_240306/Ex_04_abstract_1_main.java
+++ 240228_JavaProject/src/ex_240306/Ex_04_abstract_1_main.java
@@ -3,7 +3,13 @@ package ex_240306;
 public class Ex_04_abstract_1_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Idol_abstract idol_abstract = new Idol_abstract();
+		// -> 추상 클래스는 객체를 생성, 즉 인스턴스화 할 수 없다.
+		// 추상 클래스부터 완전히 설계도 처럼 사용
+		// 클래스 공장 -> 제품
+		// 추상 클래스 : 예) Idol -> 전반적인(추상적인) 내용만 있고,
+		// 구체화는 자식 클래스에서 재정의하여 사용한다.
+		
 
 	}
 
