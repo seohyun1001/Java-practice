017f45e75610ae0481df97b165a9c1f3cccdcc7f
seohyun1001 <sh54991210@gmail.com>
2024-03-08 12:44:17 +0900

    12:40 / [java] string buffer basic

--- 240228_JavaProject/src/ex_240308/Ex_03_StringBuffer.java
+++ 240228_JavaProject/src/ex_240308/Ex_03_StringBuffer.java
@@ -27,6 +27,54 @@ public class Ex_03_StringBuffer {
 			System.out.println("str3Concat System.identityHashCode : " + System.identityHashCode(str3Concat‎));
 			System.out.println("str4Concat System.identityHashCode : " + System.identityHashCode(str4Concat‎));
 
+
+			System.out.println();
+
+			
+			// 문제점을 개선한 StringBuffer 사용해보기
+			StringBuffer str1 = new StringBuffer("HELLO");
+			int str1HashCode = System.identityHashCode(str1);
+			
+			System.out.println("str1 : " + str1);
+			System.out.println("str1 HashCode : " + str1HashCode);
+
+
+			System.out.println();
+
+				
+			StringBuffer testAppend = str1.append(" WORLD");
+			System.out.println("Add str1.append(\" WORLD\"); : " + testAppend);
+
+			System.out.println("==== After adding append ====");
+			System.out.println("str1 : " + str1);
+			int str1HashCode2 = System.identityHashCode(str1);
+			System.out.println("str1 HashCode : " + str1HashCode2);
+
+
+			System.out.println();
+
+						
+			// 메서드 기능 간단히 확인
+			// 현재 str1 = HELLO WORLD
+			StringBuffer testInsert = str1.insert(5, " MY");
+			System.out.println("Add str1.insert(5, \" MY\"); : " + testInsert);
+			
+			System.out.println("==== After adding insert ====");
+			System.out.println("str1 : " + str1);
+			int str1HashCode3 = System.identityHashCode(str1);
+			System.out.println("str1 HashCode : " + str1HashCode3);
+			
+
+			System.out.println();
+
+						
+			StringBuffer testReplace = str1.replace(1, 5, "I");
+			System.out.println("Add str1.replace(1, 5, \"I\"); : " + testReplace);
+			System.out.println("==== After adding replace ====");
+			System.out.println("str1 : " + str1);
+			int str1HashCode4 = System.identityHashCode(str1);
+			System.out.println("str1 HashCode : " + str1HashCode4);
+			
 	}
 
 }
