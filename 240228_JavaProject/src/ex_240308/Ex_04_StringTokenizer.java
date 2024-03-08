d204bd99f714a43410be8c624bf38e2aa753ed2a
seohyun1001 <sh54991210@gmail.com>
2024-03-08 14:18:29 +0900

    14:15 / [java] string tokenizer basic_2

--- 240228_JavaProject/src/ex_240308/Ex_04_StringTokenizer.java
+++ 240228_JavaProject/src/ex_240308/Ex_04_StringTokenizer.java
@@ -14,22 +14,40 @@ public class Ex_04_StringTokenizer {
 		int n = st.countTokens();
 		System.out.println("분리된 문자열의 전체 개수 : " + n);
 		
+		System.out.println();
+		
 		System.out.println("while문으로 값의 존재유무 확인 후");
 		System.out.println("다음 토큰 값 호출 메서드로 확인");
-		while (st.hasMoreTokens()) {
-			String nextString = st.nextToken();
-			System.out.println("출력하기 : " + nextString);
-		}
-//		String query = "name=ksh&addr=busan&age=26";
+		showStringTokenizer(st);
+		
+		
+		System.out.println();
+		
+		
+		// String query = "name=ksh&addr=busan&age=26";
 		StringTokenizer st2 = new StringTokenizer(query, "&=");
 		System.out.println("while문으로 값의 존재유무 확인 후");
 		System.out.println("다음 토큰 값 호출 메서드로 확인");
-		while (st.hasMoreTokens()) {
-			String nextString2 = st2.nextToken();
-			System.out.println("출력하기 : " + nextString2);
-		}
+		showStringTokenizer(st2);
 		
 
+		System.out.println();
+		
+		
+		String query2 = "\"apple\":\"사과\", \"banana\":\"바나나\", \"grape\":\"포도\"";
+		// key : value
+		System.out.println(query2);
+		StringTokenizer st3 = new StringTokenizer(query2, ",");
+		showStringTokenizer(st3);
+		
 	}
-
+	
+	// 전역 메소드 만들기 (입력 타입 : StringTokenizer) / 단순 출력만
+	public static void showStringTokenizer(StringTokenizer stringTokenizer) {
+	while (stringTokenizer.hasMoreTokens()) {
+		String nextString = stringTokenizer.nextToken();
+		System.out.println("출력하기 : " + nextString);
+		}
+	}
+	
 }
